aea19cae42debe75fd074378e911efc8be9edceb
Henry Nguyen <henrylinh94@yahoo.de>
2024-06-06 22:11:17 +0200

    added first Rock Paper Scissors game

---
+++ rockPaperScissors.js
@@ -0,0 +1,28 @@
+//Array mit den Optionen
+const moves = ["rock", "paper", "scissors"];
+
+//User Input
+const playersChoice = process.argv.slice(2)[0].toLowerCase(); 
+console.log(`You chose ${playersChoice}`);
+
+//Computer wählt hier zufällig aus dem moves array
+const computerChoice = moves[Math.floor(Math.random() * 3)];
+console.log(`The computer chose ${computerChoice}.`);
+
+//Funktion ermittelt den Sieger
+const game = (playersChoice, computerChoice) => {
+    if (playersChoice === computerChoice) {
+      console.log("It's a tie!");
+    } else if (
+      (playersChoice === "rock" && computerChoice === "scissors") ||
+      (playersChoice === "paper" && computerChoice === "rock") ||
+      (playersChoice === "scissors" && computerChoice === "paper")
+    ) {
+      console.log("You win!");
+    } else {
+      console.log("You lose!");
+    }
+  };
+
+//Funktionsaufruf mit den beiden Parametern
+game(playersChoice, computerChoice);
